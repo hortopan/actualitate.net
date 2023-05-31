c8dc721bbb4c4f225c257205ffe685944562223e
Alex Hortopan <alex@hortopan.com>
2023-05-31 12:19:02 +0300

    sma

--- src/lib/blocks/amp/list.svelte
+++ src/lib/blocks/amp/list.svelte
@@ -3,3 +3,14 @@
 </script>
 
 {@html html}
+
+<style lang="scss">
+  :global {
+    li {
+      margin-bottom: 1em;
+      line-height: 1.5em;
+      font-size: 1.1em;
+      word-break: break-word;
+    }
+  }
+</style>
